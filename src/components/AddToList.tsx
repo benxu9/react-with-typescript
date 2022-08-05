19ac3cefc7644cc97dec256eca69bc0a54a8e793
ben <benxu9@gmail.com>
2022-08-05 23:43:08 +0930

    cool

---
+++ src/components/AddToList.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const AddToList = () => {
+    return (
+        <div className="AddToList">
+            <input
+            type="text"
+            placeholder="Name"
+            className="AddToList-Input"/>
+            <input
+            type="text"
+            placeholder="Name"
+            className="AddToList-Input"/>
+            <input
+            type="text"
+            placeholder="Name"
+            className="AddToList-Input"/>
+            <input
+            type="text"
+            placeholder="Name"
+            className="AddToList-Input"/>
+        </div>
+    )
+}
+
+export default AddToList
